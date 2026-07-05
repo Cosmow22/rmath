b08f64a02015be3c83b0da06899f1260cebe74c3
Cosmow22 <cosmow543@gmail.com>
2026-07-05 14:21:35 +0200

    add Conway function

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,13 @@ use std::io;
 // use squareroot::squareroot;
 
 mod others;
-use others::fib;
+use others::conway;
 
 
 fn main() {
     let mut input = String::new();
     io::stdin().read_line(&mut input).expect("failed to read line");
     let number: i32 = input.trim().parse().unwrap();
-    for n in fib(number).iter() {
-        println!("{}", n)
-    }
+    println!("{}", conway(number))
 
 }
--- src/others.rs
+++ src/others.rs
@@ -22,6 +22,29 @@ pub fn fib(n: i32) -> Vec<i32> {
     result
 }
 
+pub fn conway(n: i32) -> String {
+    let mut u = String::from("1");
+    for _ in 0..n {
+        let mut next_u = String::from("");
+        let mut i = 0;
+        let bytes = u.as_bytes();
+        while i < bytes.len() {
+            let mut count = 1;
+            while i + 1 < bytes.len() && bytes[i] == bytes[i + 1] {
+                i += 1;
+                count += 1;
+            }
+
+            next_u.push_str(&count.to_string());
+            next_u.push(bytes[i] as char);
+            
+            i += 1;
+        }
+        u = next_u;
+    }
+    u
+}
+
 pub fn dividers(n: i32) -> Vec<i32> {
     //for div in dividers(number).iter() {
     //println!("{}", div)
