cbd3f4dba8f1b384c1c7c608156d5862e4de0bf7
Cosmow22 <cosmow543@gmail.com>
2026-07-05 13:31:11 +0200

    add Fibonacci function

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,15 @@ use std::io;
 // use squareroot::squareroot;
 
 mod others;
-use others::dividers;
+use others::fib;
 
 
 fn main() {
     let mut input = String::new();
     io::stdin().read_line(&mut input).expect("failed to read line");
-    for div in dividers(number).iter() {
-        println!("{}", div)
+    let number: i32 = input.trim().parse().unwrap();
+    for n in fib(number).iter() {
+        println!("{}", n)
     }
 
 }
--- src/others.rs
+++ src/others.rs
@@ -7,6 +7,20 @@ pub fn fac(n: i32) -> i32 {
     }
 }
 
+pub fn fib(n: i32) -> Vec<i32> {
+    // the Fibonacci sequence
+    let mut a = 0;
+    let mut b = 1;
+
+    let mut result = Vec::new();
+
+    for _ in 0..n {
+        (a, b) = (b, a + b);
+        result.push(a);
+    }
+
+    result
+}
 
 pub fn dividers(n: i32) -> Vec<i32> {
     //for div in dividers(number).iter() {
