d797ffad5473d18657ff37eb143f8fb1b31e8c16
Cosmow22 <cosmow543@gmail.com>
2026-07-17 21:25:23 +0200

    add liebniz formula

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,4 @@
 use std::io;
-
-// mod squareroot;
-// use squareroot::squareroot;
-
-// mod sequences;
-// use sequences::conway;
-
 mod pi;
 
 
@@ -13,5 +6,5 @@ fn main() {
     let mut input = String::new();
     io::stdin().read_line(&mut input).expect("failed to read line");
     let number: i32 = input.trim().parse().unwrap();
-    println!("{}", 4.0 / (pi::brouncker(number, 1.0)-1.0))
+    println!("{}", 4.0 * pi::liebniz(number, 0))
 }
--- src/pi.rs
+++ src/pi.rs
@@ -8,3 +8,12 @@ pub fn brouncker(n: i32, k: f64) -> f64 {
     }
     return 2.0 + k*k / brouncker(n-1, k+2.0)   
 }
+
+pub fn liebniz(n: i32, k: i32) -> f64 {
+    // Estimate pi with the Liebniz forumula.
+    // Multiply by 4.0 at the end.
+    if n == 0 {
+        return (-1.0f64).powi(k) / (2*k + 1) as f64;
+    }
+    return (-1.0f64).powi(k) / (2*k + 1) as f64 + liebniz(n-1, k+1)
+}
